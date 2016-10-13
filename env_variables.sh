b52f174b979e4dc13c79d8612f6718f89dff3b46
Chris Hale <chris.hale@me.com>
2016-10-13 14:42:24 -0500

    Sample env variables

---
+++ env_variables.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+export sails_bugzilla__username=""
+export sails_bugzilla__password=""
+export sails_bugzilla__url=""
+
+export sails_github__github_token=""
+export sails_github__repo=""
